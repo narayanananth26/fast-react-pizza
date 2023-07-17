330981a50a3725fdd3c85e20c56d2c32993840e8
Ananth Narayan <narayanananth26@gmail.com>
2023-07-17 15:01:17 +0530

    config prettier for tailwind

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,13 +1,16 @@
 function Home() {
-  return (
-    <div>
-      <h1>
-        The best pizza.
-        <br />
-        Straight out of the oven, straight to you.
-      </h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1
+				className="text-xl text-yellow-500 
+      font-semibold"
+			>
+				The best pizza.
+				<br />
+				Straight out of the oven, straight to you.
+			</h1>
+		</div>
+	);
 }
 
 export default Home;
